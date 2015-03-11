3bbeab80c8d2f23b1d5f2e637ef16ed5d38ebd12
ablease <alexblease@gmail.com>
2015-03-11 17:08:05 +0000

    Template Caching: store the call to ParseFiles() in a variable templates

--- wiki.go
+++ wiki.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 )
 
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 type Page struct {
 	Title string
 	Body  []byte
@@ -64,12 +66,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServiceError)
-		return
-	}
-	err = t.Execute(w, p)
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServiceError)
 	}
